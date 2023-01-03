fe2fd665925c44722664ebdeaa3389382ecdac92
leticea <nascimento.leticeah@gmail.com>
2023-01-03 11:39:23 -0300

    mostrando os cards na tela

--- movie_library/src/components/MovieCard.jsx
+++ movie_library/src/components/MovieCard.jsx
@@ -9,9 +9,13 @@ const MovieCard = ({ movie, showLink = true }) => {
     <div className="movie-card">
       <img src={imageUrl + movie.poster_path} alt={movie.title} />
       <h2>{movie.title}</h2>
+      <p>
+        <FaStar /> {movie.vote_average}
+      </p>
+      {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
   )
   
 };
 
-export default MovieCard
+export default MovieCard;
