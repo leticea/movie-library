2c632ac2aedc47a70ad4fc6cb44458dfc92f9272
leticea <nascimento.leticeah@gmail.com>
2023-01-03 16:51:34 -0300

    criando método para pegar o detalhe do filme

--- movie_library/src/pages/Home.jsx
+++ movie_library/src/pages/Home.jsx
@@ -9,7 +9,7 @@ const apiKey = import.meta.env.VITE_API_KEY;
 const Home = () => {
   const [topMovies, setTopMovies] = useState([]);
 
-  const getTopRatedMovies = async (url) => {
+  const getTopRatedMovies = async(url) => {
     const res = await fetch(url);
     const data = await res.json();
 
--- movie_library/src/pages/Movie.jsx
+++ movie_library/src/pages/Movie.jsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} from 'react-icons/bs';
+
+import MovieCard from '../components/MovieCard';
+import './Movie.css';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
+  const {id} = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovie(data);
+  }
+
+  
+
   return <div>Movie</div>
 };
 
