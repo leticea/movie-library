8bfc960d0b5e73767a69ffaaa92e8fc29f580f65
leticea <nascimento.leticeah@gmail.com>
2023-01-03 16:26:35 -0300

    mostrando o resultado da pesquisa na tela

--- movie_library/src/pages/Search.jsx
+++ movie_library/src/pages/Search.jsx
@@ -13,12 +13,25 @@ const Search = () => {
   const [movies, setMovies] = useState([]);
   const query = searchParams.get("q");
 
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+
+    getSearchedMovies(searchWithQueryURL);
+  }, []);
+
   return (
     <div className="container">
       <h2 className="title">Resultados para: <span className="query-text">{query}</span></h2>
       <div className="movies-container">
         {movies.length === 0 && <p>Carregando...</p>}
-        {movies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   )
