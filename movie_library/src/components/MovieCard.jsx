66348a29ff3d01b313aa9a91b346f37feed91583
leticea <nascimento.leticeah@gmail.com>
2023-01-03 11:23:35 -0300

    criando movieCard

--- movie_library/src/components/MovieCard.jsx
+++ movie_library/src/components/MovieCard.jsx
@@ -1,9 +1,17 @@
 import { Link } from 'react-router-dom';
 
-const MovieCard = () => {
+import { FaStar } from 'react-icons/fa';
+
+const imageUrl = import.meta.env.VITE_IMG;
+
+const MovieCard = ({ movie, showLink = true }) => {
   return (
-    <div>MovieCard</div>
+    <div className="movie-card">
+      <img src={imageUrl + movie.poster_path} alt={movie.title} />
+      <h2>{movie.title}</h2>
+    </div>
   )
+  
 };
 
 export default MovieCard
